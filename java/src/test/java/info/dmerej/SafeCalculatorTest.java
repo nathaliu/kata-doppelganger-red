ebb1cb89370f68e3c930118e50aabef2fa2fcca7
Thomas Song <thomas.song@efrei.net>
2024-11-26 14:54:36 +0100

    SafeCalculatorTest tested

--- java/src/test/java/info/dmerej/SafeCalculatorTest.java
+++ java/src/test/java/info/dmerej/SafeCalculatorTest.java
@@ -1,10 +1,19 @@
 package info.dmerej;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class SafeCalculatorTest {
+  private final SafeCalculator calculator = new SafeCalculator(() -> true);
   @Test
   void should_not_throw_when_authorized() {
     // TODO: write a test to demonstrate the bug in SafeCalculator.add()
+    // Arrange
+    int a = 1, b = 2;
+    int expected = 3;
+    // Act
+    int result = calculator.add(a, b);
+    // Assert
+    Assertions.assertEquals(expected, result,"The calculator should return the correct sum");
   }
 }
